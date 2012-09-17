b7f410037fd857ad80e57525b8c7fe6eae661fc0
shinsato <shinsatoc@gmail.com>
2012-09-16 19:36:00 -0600

    Added random attributes to FBFriend

--- classes/FBFriend.php
+++ classes/FBFriend.php
@@ -19,13 +19,13 @@
 			if($level < 1) $this->level = 1; else $this->level = $level;
 			if($bmi < 1) $this->bmi = 1; else $this->bmi = $bmi;
 			if($thac0 < 1) $this->thac0 = 1; else $this->thac0 = $thac0;
-			if($ac < 1) $this->ac = 10; else $this->ac = $ac;
-			if(!isset($charisma)) $this->charisma = 1; else $this->charisma = $charisma;
-			if(!isset($dexterity)) $this->dexterity = 1; else $this->dexterity = $dexterity;
-			if(!isset($wisdom)) $this->wisdom = 1; else $this->wisdom = $wisdom;
-			if(!isset($intelligence)) $this->intelligence = 1; else $this->intelligence = $intelligence;
-			if(!isset($strength)) $this->strength = 1; else $this->strength = $strength;
-			if(!isset($constitution)) $this->constitution = 1; else $this->constitution = $constitution;
+			if($ac < 1) $this->ac = rand(5,10); else $this->ac = $ac;
+			if(!isset($charisma)) $this->charisma = rand(1,10); else $this->charisma = $charisma;
+			if(!isset($dexterity)) $this->dexterity = rand(1,10); else $this->dexterity = $dexterity;
+			if(!isset($wisdom)) $this->wisdom = rand(1,10); else $this->wisdom = $wisdom;
+			if(!isset($intelligence)) $this->intelligence = rand(1,10); else $this->intelligence = $intelligence;
+			if(!isset($strength)) $this->strength = rand(1,10); else $this->strength = $strength;
+			if(!isset($constitution)) $this->constitution = rand(1,10); else $this->constitution = $constitution;
 			if(!isset($warcries)) $this->warcries = array(); else $this->warcries = $warcries;
 		}
 	}
